c84bfa14f56b478e904384506932e2fdce1df8be
2301_80860582 <2301_80860582@noreply.gitcode.com>
2025-10-20 09:21:56 +0800

    feat:admin init

---
+++ business-behavior-monitor-admin/src/test/java/com/tkck/monitor/test/ApiTest.java
@@ -0,0 +1,13 @@
+package com.tkck.monitor.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class ApiTest {
+    @Test
+    public void test_ognl() {
+
+
+    }
+}
